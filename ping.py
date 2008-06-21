6e6a9390ec7a6659c4299a01e316a5432c615169
Tomas Horacek <tomas.horacek@gmail.com>
2008-06-21 17:41:30 +0200

    Pridana podpora polozek nezarovnanych na cely byte. Pridano pocitani packetu.

--- ping.py
+++ ping.py
@@ -12,19 +12,26 @@ from wrappers import Ethernet, IPv4
 
 LOCAL_DEVICE = None
 LOCAL_MAC_ADDRESS = None
+PACKET_COUNT = 1
 
 def ip_packet_callback(eth_packet, ip_packet):
+    print 'packet #%d:' % PACKET_COUNT
     print eth_packet
     print ip_packet.__str__(level = 1)
 
 def arp_packet_callback(arp_packet):
+    print 'packet #%d:' % PACKET_COUNT
     print 'ARP packet:', arp_packet
 
 def eth_packet_callback(eth_packet):
+    global PACKET_COUNT
+    
     if eth_packet.type == Ethernet.TYPE_IP:
         ip_packet_callback(eth_packet, IPv4(eth_packet.payload))
     elif eth_packet.type == Ethernet.TYPE_ARP:
         arp_packet_callback(eth_packet)
+    
+    PACKET_COUNT += 1
     
 def my_listen(fd, packet_callback):
     # fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK) # nonblocking reading - slows down CPU
@@ -50,7 +57,7 @@ def my_listen(fd, packet_callback):
 def main():
     if len(sys.argv) != 3:
         if len(sys.argv) == 1:
-            sys.argv.extend(['en1', '192.168.1.106'])
+            sys.argv.extend(['en0', '192.168.1.10'])
             print 'using defaul args:', ' '.join(sys.argv)
         else:
             print 'usage: %s dev host' % sys.argv[0]
@@ -63,17 +70,13 @@ def main():
     print 'LOCAL_MAC_ADDRESS: %s' % LOCAL_MAC_ADDRESS
     
     fd = -1
-    try:
-        fd = bpf.bpf_new()
-        bpf.bpf_set_immediate(fd, 1)
-        bpf.bpf_setif(fd, LOCAL_DEVICE)
-        
-        my_listen(fd, eth_packet_callback)
-        
-        bpf.bpf_dispose(fd)
-    except Exception, e:
-        print e
-        bpf.bpf_dispose(fd)
-
+    fd = bpf.bpf_new()
+    bpf.bpf_set_immediate(fd, 1)
+    bpf.bpf_setif(fd, LOCAL_DEVICE)
+    
+    my_listen(fd, eth_packet_callback)
+    
+    bpf.bpf_dispose(fd)
+    
 if __name__ == '__main__':
     main()
--- wrappers.py
+++ wrappers.py
@@ -8,6 +8,18 @@ class Wrapper(object):
     def __init__(self, data=None):
         '''
         Processes '_fields_' list and inits object's argumets by vals defined in data.
+        
+         * polozky nezarovana na cele byty je mozne take nacist
+            * staci zvolit rozsah na cely pocet bytu
+            * jmena poli spojit pomoci retezce __and__
+            * jako typ predat n-tici dvojic, kde:
+                * polozka n-tice odpvida prislusne polozce
+                * prvni cast dvojice je pocet bitu
+                * druha cast dvojice je typ polozky
+            * pr.:
+                ('flags__and__fragment_offset', '2s', ( # nacteme 2 byty
+                    (3, fields.HexInt(1)),              # 'flags' ma 3 bity a je typu HexInt
+                    (13, int))),                        # 'fragment_offset' je 13-bitovy integer
         '''
         if data:
             data_index = 0
@@ -16,11 +28,22 @@ class Wrapper(object):
                 unpack_str = field[1]
                 _type = field[2]
                 size = struct.calcsize(unpack_str)
-            
                 raw_val = struct.unpack(unpack_str, data[data_index:data_index + size])[0]
-                val = _type(raw_val)
-            
-                self.__setattr__(name, val)
+                
+                splitted_names = name.split('__and__')
+                if len(splitted_names) > 1:
+                    start_bit = size * 8
+                    for field_name, (num_bits, splitted_type) in zip(splitted_names, _type):
+                        shift = start_bit - num_bits
+                        splitted_raw_val = (raw_val >> shift) % (1 << num_bits)
+                        
+                        val = splitted_type(splitted_raw_val)
+                        self.__setattr__(field_name, val)
+                     
+                        start_bit -= num_bits
+                else:                
+                    val = _type(raw_val)
+                    self.__setattr__(name, val)
             
                 data_index += size
     
@@ -30,10 +53,11 @@ class Wrapper(object):
         field_separator = '\n' + level_separator * (level + 1)
         
         for field in self._fields_:
-            name = field[0]
+            unsplitted_name = field[0]
             
-            field_str = "%s=%s" % (name, self.__getattribute__(name))
-            fields_arr.append(field_str)
+            for name in unsplitted_name.split('__and__'):
+                field_str = "%s=%s" % (name, self.__getattribute__(name))
+                fields_arr.append(field_str)
         
         return level_separator * level + '<%s %s>' % (
             self.__class__.__name__,
@@ -58,12 +82,16 @@ class Ethernet(Wrapper):
 class IPv4(Wrapper):
     
     _fields_ = [
-        ('version__and__header_length', 'B', fields.HexInt(2)),
+        ('version__and__header_length', 'B', (
+            (4, int),
+            (4, int))),
         ('type_of_service', 'B', fields.HexInt(2)),
-        ('total_length', '!H', fields.HexInt(4)),
+        ('total_length', '!H', int),
         ('identification', '!H', fields.HexInt(4)),
-        ('flags__and__fragment_offset', '!H', fields.HexInt(4)),
-        ('time_to_live', 'B', fields.HexInt(2)),
+        ('flags__and__fragment_offset', '!H', (
+            (3, fields.HexInt(1)),
+            (13, int))),
+        ('time_to_live', 'B', int),
         ('protocol', 'B', fields.HexInt(2)),
         ('header_checksum', '!H', fields.HexInt(4)),
         ('saddr', '!L', fields.IPAddress),
@@ -71,4 +99,4 @@ class IPv4(Wrapper):
     ]
     
 
-print Ethernet('\x01\x80\xc2\x00\x00\x00\x00\x1a\x92\x62\x31\x4c\x00\x2e\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00\x00\x1a\x92\x62\x31\x4c\x01\x80\xc2')
+#print Ethernet('\x01\x80\xc2\x00\x00\x00\x00\x1a\x92\x62\x31\x4c\x00\x2e\x42\x42\x03\x00\x00\x00\x00\x00\x80\x00\x00\x1a\x92\x62\x31\x4c\x01\x80\xc2')
